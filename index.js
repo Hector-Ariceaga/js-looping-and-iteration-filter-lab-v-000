2d3bf0deb1bd5fbf3c92ebed41018ef0a8c5b3bc
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-09 08:01:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function findMatching(drivers, string) {
-console.log(string.toLowerCase())
   const matches = drivers.filter(function(driver) {return driver.toLowerCase() === string.toLowerCase()})
   return matches
 }
+
